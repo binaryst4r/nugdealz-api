3e457115ffcf808cc516d5135e45aedd2cc33451
Colin Kuhn <colin.kuhn2@gmail.com>
2015-05-19 09:18:14 -0400

    install ez

--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 20150510062021) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.float    "price"
-    t.boolean  "medical"
-    t.boolean  "recreational"
+    t.boolean  "medical",            default: false
+    t.boolean  "recreational",       default: false
   end
 
   create_table "dispensaries", force: true do |t|
@@ -35,18 +35,18 @@ ActiveRecord::Schema.define(version: 20150510062021) do
     t.string   "zip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                                  null: false
+    t.string   "encrypted_password",                     null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                          null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.boolean  "recreational"
-    t.boolean  "medical"
+    t.boolean  "recreational",           default: false
+    t.boolean  "medical",                default: false
     t.float    "latitude"
     t.float    "longitude"
   end
@@ -66,8 +66,8 @@ ActiveRecord::Schema.define(version: 20150510062021) do
     t.string   "zip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "medical"
-    t.boolean  "recreational"
+    t.boolean  "medical",         default: false
+    t.boolean  "recreational",    default: false
     t.boolean  "verified",        default: false
   end
 
@@ -96,12 +96,12 @@ ActiveRecord::Schema.define(version: 20150510062021) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                                  null: false
+    t.string   "encrypted_password",                     null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",                          null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
